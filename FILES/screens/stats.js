496fe2e703112df954ff89ffb16679f2d6e8aaa1
Katia Bellido <katiabellido@MacBook-Pro-de-Katia.local>
2021-05-05 21:34:45 -0500

    Dudas con stats

--- FILES/screens/stats.js
+++ FILES/screens/stats.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View, Switch, Image  } from 'react-native';
-import ActivityRings from "react-native-activity-rings";  
+import {Component } from 'react';
+import { StyleSheet, Text, View, ScrollView,Button, Alert, ActivityIndicator } from 'react-native';
+import ActivityRings from "react-native-activity-rings"; 
+import { HeaderMenu } from "./HeaderMenu";
  
-export const Stats = () => {
+const Stats = () => {
  
  const activityData = [
     {
+      label: "Mes",
       value: 0.8, // ring will use color from theme
+      color: "#023047",
     },
     {
-      label: "ACTIVITY",
-      value: 0.6,
-      color: "#cb5f18",
+        label: "Semana",
+        value: 0.6,
+        color: "#219EBC",
     },
     {
-      label: "RINGS",
-      value: 0.2,
-      color: "#86040f",
-      backgroundColor: "#cccccc"
+        label: "Día",
+        value: 0.2,
+        color: "#4D0C5A",
     }
   ];
  
@@ -28,7 +31,64 @@ export const Stats = () => {
  
  return (
   <View>
-    <ActivityRings data={activityData} config={activityConfig} /> 
+    <ActivityRings theme={"light"} legend = {true} data={activityData} config={activityConfig} /> 
   </View>
   );
 }  
+
+export default class Statistics extends Component {
+    render() {
+        return (
+            <View style={styles.container}> 
+                <HeaderMenu/>
+                <View style={styles.containerSafe}>
+                    {/* <ActivityIndicator/> */}
+                    <ScrollView>
+                    <Text style={ styles.Title}>
+                        Productividad (entrar todos los días del mes)
+                    </Text>
+                    <Stats/>
+                    <Text style={ styles.Title}>
+                        Peso Cargado
+                    </Text>
+                    <Text style={ styles.Title}>
+                        minutos
+                    </Text>
+                    <Button
+                        title="Go back"
+                        color="#219EBC"
+                        onPress={() => Alert.alert('Regresa si quieres')}
+                        />
+                    </ScrollView>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgba(105,109,125, 0.1)",
+    },
+    containerSafe: {
+      flex: 1,
+      marginHorizontal: 16,
+    },
+    image:{
+        margin: 16,
+        padding: 20,
+        width: 80, 
+        height:80,
+    },
+    Title: {
+        padding: 10,
+        fontSize: 16,
+        color: "#023047",
+        fontWeight: "bold",
+        textAlign: "center",
+        
+    },
+    
+  });
+  
